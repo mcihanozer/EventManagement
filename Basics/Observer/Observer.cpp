2d652e0ee2bad9eef0f9bcd980610b21fdc811f9
M. Cihan Ozer <ozer.mc@gmail.com>
2018-03-28 19:49:18 +0300

    Create Observer.cpp

---
+++ Basics/Observer/Observer.cpp
@@ -0,0 +1,19 @@
+//
+//  Observer.cpp
+//
+//  Created by Cihan Ozer (www.mcihanozer.com)
+//  Copyright © 2017 Cihan Ozer. All rights reserved.
+//
+
+#include "Observer.hpp"
+
+Observer::Observer()
+    : Object()
+{
+    
+}
+
+Observer::~Observer()
+{
+    
+}
