83964408be332bb0ed436051118ac3bbe8223235
M. Cihan Ozer <ozer.mc@gmail.com>
2018-03-28 19:42:18 +0300

    Create ActionEventManager.cpp

---
+++ Input-Events/EventManagers/ActionEventManager.cpp
@@ -0,0 +1,18 @@
+//
+//  ActionEventManager.cpp
+//
+//  Created by Cihan Ozer (www.mcihanozer.com)
+//  Copyright © 2017 Cihan Ozer. All rights reserved.
+//
+
+#include "ActionEventManager.hpp"
+
+ActionEventManager::ActionEventManager()
+{
+    
+}
+
+ActionEventManager::~ActionEventManager()
+{
+    
+}
