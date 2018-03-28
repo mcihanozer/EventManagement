da9d33907f33096394c796283fc6a7a52f8245da
M. Cihan Ozer <ozer.mc@gmail.com>
2018-03-28 19:43:30 +0300

    Create MouseEventManager.cpp

---
+++ Input-Events/EventManagers/MouseEventManager.cpp
@@ -0,0 +1,18 @@
+//
+//  MouseEventManager.cpp
+//
+//  Created by Cihan Ozer (www.mcihanozer.com)
+//  Copyright © 2017 Cihan Ozer. All rights reserved.
+//
+
+#include "MouseEventManager.hpp"
+
+MouseEventManager::MouseEventManager()
+{
+    
+}
+
+MouseEventManager::~MouseEventManager()
+{
+    
+}
